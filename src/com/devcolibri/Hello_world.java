6c8096d6d56caa181a6afbef295f32f1b83917d3
Serginio777 <sergiluxa@mail.ru>
2018-01-15 16:38:44 +0300

    test

--- src/com/devcolibri/Hello_world.java
+++ src/com/devcolibri/Hello_world.java
@@ -3,5 +3,6 @@ package com.devcolibri;
 public class Hello_world {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello");
     }
 }
